9888214417351ffc12d4dd136c7497e80fd03d13
andrushechkaL <lynovskiy21@gmail.com>
2019-11-18 12:28:26 +0200

    add pagination

--- src/main/java/com/example/netbooks/controllers/BookController.java
+++ src/main/java/com/example/netbooks/controllers/BookController.java
@@ -3,29 +3,45 @@ package com.example.netbooks.controllers;
 import com.example.netbooks.dao.JdbcBookRepository;
 import com.example.netbooks.models.Announcement;
 import com.example.netbooks.models.Book;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://netbooksfront.herokuapp.com")
+@CrossOrigin(origins = {"http://localhost:4200", "https://netbooksfront.herokuapp.com"})
+@RequestMapping(value = "/book-service")
 public class BookController {
+    private final Logger logger = LogManager.getLogger(AuthenticationController.class);
     @Autowired
     private JdbcBookRepository jdbcBookRepository;
 
-    @RequestMapping(value="/home/books", method = RequestMethod.GET)
+    @GetMapping(value="/home/books")
     public List<Book> getAllBooks() {
         return jdbcBookRepository.findAll();
     }
 
-    @RequestMapping(value="/home/books/addBook", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    @PostMapping("/home/books/addBook")
     public String add (@RequestBody Book book){
         return jdbcBookRepository.addBook(book);
     }
 
-    @RequestMapping(value="/home/announcement", method = RequestMethod.GET)
+    @GetMapping(value="/home/announcement")
     public List<Announcement> getAllAnnouncement() {
         return jdbcBookRepository.findAllAnnouncement();
+    }
+
+    @GetMapping(value="/home/amountOfAnnouncement")
+    public int getAmountOfAnnouncement() {
+        return jdbcBookRepository.getAmountOfAnnouncement();
+    }
+
+    @GetMapping(value="/home/announcementListPeace")
+    public List<Announcement> getPeaceAnnouncement(@RequestParam("page")int page, @RequestParam("booksPerPage")int booksPerPage) {
+        logger.info("page {} booksPerPage {}",page, booksPerPage);
+        System.out.print(page + ' ' + booksPerPage);
+        return jdbcBookRepository.getPeaceAnnouncement(page,booksPerPage);
     }
 }
